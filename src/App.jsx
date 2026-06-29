2de28ac2ffc558159fa07e2451b938a3ce09987c
haranadh03 <haranadhchowdary1436@gmail.com>
2026-06-29 23:34:09 +0530

    newfiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Navbar from "./Navbar"
-
+import Navbar from "./Navbar";
+import 'animate.css';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <h1 className="animate__animated animate__rubberBand">Hello world</h1>
     </>
 
   )
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,15 +1,13 @@
 import wave from './assets/wave-haikei.png';
+import './App.css';
+import "Animate.css";
 
 function Contact(){
-    const styles = {
-        backgroundImage:`url(${wave})`,
-        height:'100vh',
-        backgroundSize:"cover",
-        backgroundRepeat:"no-repeat"
-    }
+
     return (
         <>
-        <div style={styles}>
+        <div className='contact'>
+            <h1 className='animate__heartBeat'>Login Form</h1>
             <input type="text" placeholder='EnterYournameplease'/><br />
             <input type="text" placeholder='ProvideEmail'/><br />
         </div>
